f13a9786c7a66fcdc2ad3bf3378a431fe0141a75
Sang Hun Lee <124578663+Sanghun0901@users.noreply.github.com>
2023-03-06 01:56:12 +0900

    Create 백준 17219번 (비밀번호 찾기).py

---
+++ 이상훈 코드모음/백준 17219번 (비밀번호 찾기).py
@@ -0,0 +1,21 @@
+import queue
+num, ber = map(int, input().split())
+save = {}
+q = queue.deque([])
+for i in range(num):
+    site, pw = input().split()
+    save[site] = pw
+
+find = []
+
+for i in range(ber):
+    site = str(input())
+    q.append(site)
+
+while q:
+    site = q.popleft()
+    if site in save:
+        find.append(save[site])
+
+for pw in find:
+    print(pw)
