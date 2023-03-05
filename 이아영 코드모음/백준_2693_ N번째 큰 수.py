d023c152c1d527d0d31abae8a8c53e0c84e14864
dkdud1871 <48914911+dkdud1871@users.noreply.github.com>
2023-03-06 02:01:33 +0900

    Create 백준_2693_ N번째 큰 수.py

---
+++ 이아영 코드모음/백준_2693_ N번째 큰 수.py
@@ -0,0 +1,9 @@
+# 2693번 N번째 큰 수
+n= int(input())
+arr=[]
+for _ in range(n):
+    arr.append(sorted(list(map(int, input().split()))))
+a=[]
+for i in range(n):
+    a.append(arr[i][-3])
+print(*a, sep='\n')
