24611d3968803c19b04548b5479e7036c260d4b8
dkdud1871 <48914911+dkdud1871@users.noreply.github.com>
2023-04-10 14:43:32 +0900

    Create 백준_24444_알고리즘 수업-너비우선탐색1.py

---
+++ 이아영 코드모음/백준_24444_알고리즘 수업-너비우선탐색1.py
@@ -0,0 +1,36 @@
+from collections import deque
+import sys
+  
+n,m,r= map(int, sys.stdin.readline().split())
+visited= [0]*(n+1)
+graph= [[] for _ in range(n+1)]
+count=1
+
+
+for _ in range(m): # 간선 수를 받아와야 한다.
+    a,b= map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    
+for i in range(n+1):
+    graph[i].sort()
+    
+def bfs(graph, start, visited):
+    global count
+    queue= deque([start])
+    visited[start]= count
+    
+    while queue:
+        v= queue.popleft()
+        for j in graph[v]:
+            if visited[j]==0:
+                queue.append(j)
+                count+=1
+                visited[j]= count
+                
+                
+                
+bfs(graph, r, visited)
+
+for i in range(1, n+1):
+        print(visited[i])
